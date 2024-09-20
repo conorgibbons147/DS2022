e8ad7f7cbed74a30c51f6ea5f8b043467c2fbaf4
conorgibbons147 <hjd3db@virginia.edu>
2024-09-20 11:41:24 -0400

    First edit to lab 3

---
+++ my-work/lab3/madlib.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+# 8 user prompts to fill in the madlib
+read -p "1. Name an adjective: " ADJ1
+read -p "2. Name an adjective: " ADJ2
+read -p "3. Name a type of bird: " BIRD
+read -p "4. Name a place in your home: " ROOM
+read -p "5. Name a verb (past tense): " VERB1
+read -p "6. Name a verb: " VERB2
+read -p "7. Name a person: " PRSON
+read -p "8. Name a noun: " NOUN1
+read -p "9. Name a liquid: " LQD
+read -p "10. Name a verb ending in -ing: " VERB3
+read -p "11. Name a body part (plural): " BODY
+read -p "12. Name a noun (plural): " NOUN2
+read -p "13. Name a verb ending in -ing: " VERB4
+read -p "14. Name a noun: " NOUN3
+
+
